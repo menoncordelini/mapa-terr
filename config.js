0b86c65bd1ef27d96f9b97e3654b7b322e499b9a
menoncordelini <menon.cordelini@gmail.com>
2026-08-07 00:18:55 -0300

    Add Firebase configuration to config.js
    Add Firebase configuration object with necessary keys.

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA6u0NePsHWgdEnf",
+    authDomain: "://firebaseapp.com",
+    databaseURL: "https://firebaseio.com",
+    projectId: "mapa3-terr",
+    storageBucket: "mapa3-terr.firebasestorage.app",    // Armazenamento de arquivos
+    messagingSenderId: "15579279461",                   // Notificações
+    appId: "1:15579279461:web:5ce5d4705353506262968c",   // ID do app
+    // ...
+};
