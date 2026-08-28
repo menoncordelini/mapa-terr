c5c8457dfa15f8545c8a0cc531e540b4300155cd
menoncordelini <menon.cordelini@gmail.com>
2026-08-28 13:46:06 -0300

    Change databaseURL in firebaseConfig
    Updated databaseURL  firebaseio.com.  correção

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const firebaseConfig = {
     apiKey: "AIzaSyA6u0NePsHWgdEnf",
     authDomain: "://firebaseapp.com",   // "://firebaseapp.com",  "mapa3-terr.firebaseapp.com", 
-    databaseURL: "https://mapa3-terr-default-rtdb.firebase.com",   // "https://firebaseio.com",
+    databaseURL: "https://firebaseio.com",   // "https://firebaseio.com",  "https://mapa3-terr-default-rtdb.firebase.com", 
     projectId: "mapa3-terr",
     storageBucket: "mapa3-terr.firebasestorage.app",    // Armazenamento de arquivos
     messagingSenderId: "15579279461",                   // Notificações
