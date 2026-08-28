1acc07e9f9346ae65d37f58caf3a3e60e07b80a4
menoncordelini <menon.cordelini@gmail.com>
2026-08-28 12:13:45 -0300

    Fix authDomain in Firebase configuration
    Correção configuration for Firebase authDomain

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const firebaseConfig = {
     apiKey: "AIzaSyA6u0NePsHWgdEnf",
-    authDomain: "mapa3-terr.firebaseapp.com",   // "://firebaseapp.com",
+    authDomain: "://firebaseapp.com",   // "://firebaseapp.com",  "mapa3-terr.firebaseapp.com", 
     databaseURL: "https://mapa3-terr-default-rtdb.firebase.com",   // "https://firebaseio.com",
     projectId: "mapa3-terr",
     storageBucket: "mapa3-terr.firebasestorage.app",    // Armazenamento de arquivos
