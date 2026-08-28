73574f7d36604b5727a4dd69ff9b8f9084db4926
menoncordelini <menon.cordelini@gmail.com>
2026-08-28 14:08:04 -0300

    Update config for new project settings
    Nova conexão para  firebase

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
     apiKey: "AIzaSyA6u0NePsHWgdEnf",
-    authDomain: "://firebaseapp.com",   // "://firebaseapp.com",  "mapa3-terr.firebaseapp.com", 
-    databaseURL: "https://firebaseio.com",   // "https://firebaseio.com",  "https://mapa3-terr-default-rtdb.firebase.com", 
+    authDomain: "mapa3-terr.firebaseapp.com",    // "://firebaseapp.com",  "mapa3-terr.firebaseapp.com", 
+    databaseURL: "https://mapa3-terr-default-rtdb.firebase.com",   // "https://firebaseio.com",  "https://mapa3-terr-default-rtdb.firebase.com", 
     projectId: "mapa3-terr",
     storageBucket: "mapa3-terr.firebasestorage.app",    // Armazenamento de arquivos
-    messagingSenderId: "15579279461",                   // Notificações
+    messagingSenderId: "15579279461",                 // Notificações
     appId: "1:15579279461:web:5ce5d4705353506262968c",   // ID do app
     // ...
 };
